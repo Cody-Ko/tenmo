9036f6ad0ba66c9256d7a14d0cb6c6b8a255d30c
cataclysmicserendipity <jenniferdgaylord@gmail.com>
2024-05-01 11:36:09 -0500

    tweeked the service method viewBalance

--- tenmo-server/src/main/java/com/techelevator/tenmo/service/AccountService.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/service/AccountService.java
@@ -2,6 +2,7 @@ package com.techelevator.tenmo.service;
 
 import com.techelevator.tenmo.dao.AccountDao;
 import com.techelevator.tenmo.dao.JdbcUserDao;
+import com.techelevator.tenmo.dao.UserDao;
 import com.techelevator.tenmo.security.SecurityUtils;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
@@ -13,17 +14,16 @@ import java.util.Optional;
 public class AccountService {
 
     AccountDao accountDao;
-    JdbcTemplate jdbcTemplate;
+    UserDao userDao;
 
-    public AccountService(AccountDao accountDao, JdbcTemplate jdbcTemplate){
+    public AccountService(AccountDao accountDao, UserDao userDao){
         this.accountDao = accountDao;
-        this.jdbcTemplate = jdbcTemplate;
+        this.userDao = userDao;
     }
 
     public double viewBalance(){
-        JdbcUserDao jdbcUserDao = new JdbcUserDao(jdbcTemplate);
         String currentUser = SecurityUtils.getCurrentUsername().get();
-        int currentUserId = jdbcUserDao.findIdByUsername(currentUser);
+        int currentUserId = userDao.findIdByUsername(currentUser);
         return accountDao.viewBalance(currentUserId);
     }
 }
